785d678db8a2c2dccb3f597551bbf60386509b6f
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2023-05-11 04:39:29 +0300

    Update getHeaders.js

--- hooks/getHeaders.js
+++ hooks/getHeaders.js
@@ -1,7 +1,7 @@
 export default function getHeaders() {
   return {
     headers: {
-      Authorization: process.env.API_TOKEN,
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
     },
   };
 }
