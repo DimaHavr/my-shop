fd2800306aa4b4c891640fa6a1c34138795a8466
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2023-05-25 00:33:58 +0300

    update

--- components/SuccessBox/SuccessBox.js
+++ components/SuccessBox/SuccessBox.js
@@ -1,21 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button, Overlay, SuccessBoxStyled } from "./SuccessBox.styled";
 
 const SuccessBox = ({ children, setConfettiActive }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const onCloseModal = () => {
+    setIsMenuOpen(false);
+    setConfettiActive(false);
+  };
 
   return (
-    <Overlay isMenuOpen={isMenuOpen}>
+    <Overlay isMenuOpen={isMenuOpen} onClick={onCloseModal}>
       {isMenuOpen && (
         <SuccessBoxStyled isMenuOpen={isMenuOpen}>
-          <Button
-            onClick={() => {
-              setIsMenuOpen(false);
-              setConfettiActive(false);
-            }}
-          >
-            Закрити
-          </Button>
+          <Button onClick={onCloseModal}>Дякуємо за замовлення!</Button>
         </SuccessBoxStyled>
       )}
       {children}
